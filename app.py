1cb410ffbb8a7af339a7a2899f891a9f2e86ae6b
tkudinov <tkudinov90@gmail.com>
2026-04-13 17:31:15 +0300

    For Render

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -13,5 +14,5 @@ def show_ip():
     return f"Нет, вот это Вы : {user_ip} 😈 \n"
 
 if __name__ == "__main__":
-
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
